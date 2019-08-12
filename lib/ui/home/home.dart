54c7ee503e02cc1e1013e39d312caf3e46453621
Tantan4321 <kupa528@yahoo.com>
2019-08-12 15:12:28 -0500

    Home page body

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -14,7 +14,39 @@ class _HomeState extends State<Home> {
       body: new Column(
         children: <Widget>[
           new GradientAppBar(Theme.of(context).primaryColor, Theme.of(context).accentColor, Colors.black, "PF Timer"),
+          new HomePageBody()
         ],
+      ),
+    );
+  }
+}
+
+class HomePageBody extends StatefulWidget {
+  @override
+  _HomePageBodyState createState() => _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<HomePageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Container(
+        color: new Color(0xFF736AB7),
+        child: new CustomScrollView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: false,
+          slivers: <Widget>[
+            new SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              sliver: new SliverList(
+                delegate: new SliverChildBuilderDelegate(
+                      (context, index) => new PlanetSummary(planets[index]),
+                  childCount: planets.length,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
